e7a12bbaeb64592bb28e66a9557abc594bda51fd
Nadzri <nadzrisan32@gmail.com>
2024-08-11 09:24:52 +0700

    config assigment

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 // Ini Javascript
+const beratbadanInput = document.getElementById("beratbadan")
+const usiaInput = document.getElementById("usia")
+const tinggibadanInput = document.getElementById("tinggibadan")
+const hasilSpan = document.getElementById("hasil")
+
+function calculateTotal(){
+    const beratbadan = beratbadanInput.value;
+    const usia = parseInt(usiaInput.value.replace(",", ""));
+    const tinggibadan = parseInt(tinggibadanInput.value.replace(",", ""));
+    const hasil = beratbadan/tinggibadan**;
+
+    hasilSpan.innerHTML = `${hasil.toLocaleString("id")}`;
+}
+
+beratbadanInput.addEventListener("input", calculateTotal);
+tinggibadanInput.addEventListener("input", calculateTotal);
+usiaInput.addEventListener("input", calculateTotal);
+
+calculateTotal();
